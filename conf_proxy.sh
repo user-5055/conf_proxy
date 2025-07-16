65c048450756b7096136fe7ebc07c10bde775992
Bestoj <125223533+user-5055@users.noreply.github.com>
2025-07-16 16:45:26 +0200

    Update conf_proxy.sh
    Pour ajouter la MIB SOPHOS

--- conf_proxy.sh
+++ conf_proxy.sh
@@ -65,3 +65,10 @@ psk_identity="PSK-$(echo "$proxy_hostname" | tr ' ' '-')"
 sed -i "s/^# TLSPSKIdentity=/TLSPSKIdentity=$psk_identity/" "$conf_file"
 
 systemctl restart zabbix-proxy
+
+# Telechargement de la MIB SOPHOS
+curl -o SFOS-FIREWALL-MIB.txt https://mibbrowser.online/mibs/SFOS-FIREWALL-MIB.mib
+mkdir -p /usr/local/share/snmp/mibs
+touch /etc/snmp/snmp.conf
+echo "mibdirs +/usr/local/share/snmp/mibs" >> /etc/snmp/snmp.conf
+mv SFOS-FIREWALL-MIB.txt /usr/local/share/snmp/mibs
