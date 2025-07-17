48e4873598c7f1491896ef762bd498f391109272
Bestoj <125223533+user-5055@users.noreply.github.com>
2025-07-17 16:44:26 +0200

    Update conf_proxy.sh
    sophos conf changement

--- conf_proxy.sh
+++ conf_proxy.sh
@@ -68,8 +68,5 @@ systemctl restart zabbix-proxy
 
 # Telechargement de la MIB SOPHOS
 curl -o SFOS-FIREWALL-MIB.txt https://mibbrowser.online/mibs/SFOS-FIREWALL-MIB.mib
-mkdir -p /usr/local/share/snmp/mibs
-touch /etc/snmp/snmp.conf
-echo "mibdirs +/usr/local/share/snmp/mibs" >> /etc/snmp/snmp.conf
-mv SFOS-FIREWALL-MIB.txt /usr/local/share/snmp/mibs
+cp SFOS-FIREWALL-MIB.txt /usr/share/snmp/mibs
 systemctl restart zabbix-proxy
