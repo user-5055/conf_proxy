19a6168c407562b1188677c7c42264e2537d7a84
Bestoj <125223533+user-5055@users.noreply.github.com>
2025-07-17 17:02:33 +0200

    Update mib_sophos.sh

--- mib_sophos.sh
+++ mib_sophos.sh
@@ -1,7 +1,4 @@
 # Telechargement de la MIB SOPHOS
 curl -o SFOS-FIREWALL-MIB.txt https://mibbrowser.online/mibs/SFOS-FIREWALL-MIB.mib
-mkdir -p /usr/local/share/snmp/mibs
-touch /etc/snmp/snmp.conf
-echo "mibdirs +/usr/local/share/snmp/mibs" >> /etc/snmp/snmp.conf
-mv SFOS-FIREWALL-MIB.txt /usr/local/share/snmp/mibs
+cp SFOS-FIREWALL-MIB.txt /usr/share/snmp/mibs
 systemctl restart zabbix-proxy
