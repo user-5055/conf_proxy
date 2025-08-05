b2e7264b591a0e827799684198d0912f3bb3ec76
Bestoj <125223533+user-5055@users.noreply.github.com>
2025-08-05 11:11:28 +0200

    Update maj_proxy.sh

--- maj_proxy.sh
+++ maj_proxy.sh
@@ -1,11 +1,13 @@
+exec > /dev/null 2>&1
+
 read -p "Version du proxy : " proxy_version
 echo "Arrêt du proxy"
-systemctl stop zabbix-proxy > /dev/null 2> /dev/null
+systemctl stop zabbix-proxy
 echo "Ajout du dépot de Zabbix Proxy $proxy_version"
-rpm -Uvh https://repo.zabbix.com/zabbix/$proxy_version/release/alma/9/noarch/zabbix-release-latest-$proxy_version.el9.noarch.rpm > /dev/null 2> /dev/null
+rpm -Uvh https://repo.zabbix.com/zabbix/$proxy_version/release/alma/9/noarch/zabbix-release-latest-$proxy_version.el9.noarch.rpm
 echo "Nettoyage du cache des dépots..."
-dnf clean all > /dev/null 2> /dev/null
+dnf clean all
 echo "Mise à jour du proxy..."
-dnf update zabbix-* -y > /dev/null 2> /dev/null
-systemctl start zabbix-proxy > /dev/null 2> /dev/null
+dnf update zabbix-* -y
+systemctl start zabbix-proxy
 echo "Proxy redémarré"
