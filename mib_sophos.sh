3ce31186632622ea1a011854575899d7bb40a0dd
Bestoj <125223533+user-5055@users.noreply.github.com>
2025-08-05 09:28:25 +0200

    Delete mib_sophos.sh

--- mib_sophos.sh
+++
@@ -1,4 +0,0 @@
-# Telechargement de la MIB SOPHOS
-curl -o SFOS-FIREWALL-MIB.txt https://mibbrowser.online/mibs/SFOS-FIREWALL-MIB.mib
-cp SFOS-FIREWALL-MIB.txt /usr/share/snmp/mibs
-systemctl restart zabbix-proxy
